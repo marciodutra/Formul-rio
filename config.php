a9dddc6fac4648feaf1701fc7d44638abe677e85
marciodutra <78557074+marciodutra@users.noreply.github.com>
2023-12-21 05:21:24 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = '';
+$dbPassword = '';
+$dbName = 'Aqui vai o nome do banco de dados';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName)
+
+?>
